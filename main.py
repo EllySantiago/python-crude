c7ed6f9fd11821f8adb8f3c47c11150766578949
EllySantiago <--global>
2025-06-03 09:33:11 -0300

    atualizei o crud

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import json
-import os
-
 from database import ler_dados , escrever_dados
 
-
-
-usuarios = {}
 senha_geral = "1234"
 
 
@@ -23,55 +17,58 @@ def senha():
     return False
 
 def delegado_adicionar():
-    try:
-        id = int(input("Digite o ID do usuário: "))
-        if id in usuarios:
-            print("Esse ID já está em uso.")
-        else:
-            nome = input("Digite o nome do usuário: ")
-            usuarios[id] = nome
-            print(f"Usuário '{nome}' adicionado com sucesso.")
-    except ValueError:
-        print("ID inválido. Use apenas números.")
+        id = int(input("Digite a matrícula do delegado: "))
+        usuarios = ler_dados("usuario")
+        for usuario in usuarios:
+            if id == usuario['id']:
+                print("Esse ID já está em uso.")
+                break
+            else:
+                nome = input("Digite o nome do usuário: ")
+                usuarios.append({"nome": nome ,"id": id})
+                escrever_dados(usuarios, "usuario")
+                print(f"Usuário adicionado com sucesso.")
+                break
 
 def delegado_excluir():
-    try:
-        id = int(input("Digite o ID do usuário a ser excluído: "))
-        if id in usuarios:
-            nome = usuarios.pop(id)
-            print(f"Usuário '{nome}' removido com sucesso.")
+    id = int(input("Digite a matrícula do delegado a ser excluído: "))
+    usuarios = ler_dados("usuario")
+    for usuario in usuarios:
+        if id == usuario['id']:
+            usuarios.remove(usuario)
+            escrever_dados(usuarios, "usuario")
+            print(f"Usuário removido com sucesso.")
         else:
-            print("Usuário não encontrado.")
-    except ValueError:
-        print("ID inválido. Use apenas números.")
+            print(f"Usuário não encontrado.")
 
 def lista_de_delegados():
+    usuarios = ler_dados("usuario")
+    print(usuarios)
     if usuarios:
         print("\nUsuários cadastrados:")
-        for id, nome in usuarios.items():
-            print(f"ID: {id} - Nome: {nome}")
+        for usuario in usuarios:
+            print(f"ID: {usuario['id']} - Nome: {usuario['nome']}")
     else:
         print("Nenhum usuário cadastrado.")
 
 def atualizar_delegado():
-    id = input("digite o id de delegado que deseja atualizar:").strip()
-    if id in usuarios:
-        print(f'usuario atual:{usuarios[id]}')
-        novo_nome=input("digite o novo nome de usuario(aperte enter para manter):").strip()
-        if novo_nome:
-            usuarios[id]= novo_nome
-            escrever_dados(usuarios, nome_do_arquivo)
-            print("usuario atualizado com sucesso")
-        else:
-            print("nenhuma alteraçao feita.")
-    else:
-        print("usuario nao encontrado.")
+    usuarios = ler_dados("usuario")
+    id = input("digite a matricula/id de delegado que deseja atualizar:")
+    for index, usuario in enumerate(usuarios):
+        if usuario['id'] == id:
+            print(f'usuario atual: {usuario['id']}')
+            novo_id = input("digite a nova matricula/id para o delegado:")
+            novo_nome = input("digite o novo nome")
+            usuarios[index] = {"id": novo_id, "nome": novo_nome}
+            escrever_dados(usuarios, "usuario")
+            print("usuario atualizado com sucesso!")
+            break
 
-def menu():
+
+def menu_de_delegados():
     while True:
         print("█" * 50)
         print("█" * 14 + " SISTEMA DE DENÚNCIAS " + "█" * 14)
-        print("█" * 50)
         print("1. Adicionar Delegado")
         print("2. Excluir Delegado")
         print("3. Listar Delegados")
@@ -86,7 +83,7 @@ def menu():
         elif escolha == '3':
             lista_de_delegados()
         elif escolha == '4':
-            lista_de_delegados()
+           atualizar_delegado()
         elif escolha == '5':
             print("Saindo do programa.")
             break
@@ -95,7 +92,7 @@ def menu():
 
 
 if senha():
-    menu()
+    menu_de_delegados()
 
 
 
