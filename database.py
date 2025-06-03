4ef9741e592282ea835076704c4c8b5476cfea44
Lara <larathaylanne@gmail.com>
2025-06-03 10:20:06 -0300

    Update database.py
    Criando meu arquivo json

--- database.py
+++ database.py
@@ -1,5 +1,12 @@
-def main():
-    pass
+import json
 
-if __name__ == "__main__":
-    main()
+def ler_dados(nome_arquivo):
+    try:
+        with open(f"{nome_arquivo}.json", "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return[]
+
+def escrever_dados(lista, nome_arquivo):
+    with open(f"{nome_arquivo}.json", "w", encoding="utf-8") as f:
+        json.dump(lista, f, indent=4, ensure_ascii=False)
